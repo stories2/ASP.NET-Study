336f8e0f574f156af3d8a9852e776effd4312a4f
Kim HyunWoo <stories2@naver.com>
2018-07-23 17:52:31 +0900

    Models/MyProfile : create my profile data

---
+++ Tutorial/Models/MyProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Tutorial.Models
+{
+    public class MyProfile
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime updateDateTime { get; set; }
+        public string memo { get; set; }
+        public decimal age { get; set; }
+    }
+
+    public class MyProfileDBContext : DbContext
+    {
+        public DbSet<MyProfile> Profiles { get; set; }
+    }
+}
